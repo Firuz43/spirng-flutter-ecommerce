546a37af971d524d224441d067342762a3f36e26
Firuz Chamarov <firuz1199@mail.ru>
2025-10-15 19:03:33 +0400

    adding chengis to our product model

--- ecommerce/src/main/java/com/example/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/example/ecommerce/model/Product.java
@@ -5,11 +5,16 @@ public class Product {
     private int id;
     private String name;
     private double price;
+    private String description;
+    private String imageUrl;
 
-    public Product(int id, String name, double price) {
+
+    public Product(int id, String name, double price, description, imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
     } 
 
     public int getId() {
@@ -22,5 +27,13 @@ public class Product {
 
     public double getPrice() {
         return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private String getImageUrl() {
+        return imageUrl;
     }
 }
